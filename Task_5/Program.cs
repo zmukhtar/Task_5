c5f5676e8162c385c4efa936839d7cea32deaf74
zmukhtar <61983845+zmukhtar@users.noreply.github.com>
2023-03-31 12:14:28 +0300

    - Update code

--- Task_5/Program.cs
+++ Task_5/Program.cs
@@ -1,8 +1,10 @@
-﻿using static System.Math;
+﻿using System.Text;
+using static System.Math;
 // See https://aka.ms/new-console-template for more information
 int getNumberOne, getNumberTwo;
 double getDoubleOne;
 StartProgramm();
+string answerTask;
 
 void StartProgramm()
 {
@@ -36,47 +38,52 @@ void StartProgramm()
     switch (getNumberOne)
     {
         case (1):
-            Console.WriteLine("Task_4_1. Вывести все квадраты натуральных чисел, не превосходящие данного числа N.");
-            Console.WriteLine("Введите предел.");
+            Print("Task_4_1. Вывести все квадраты натуральных чисел, не превосходящие данного числа N.");
+            Print("Введите предел.");
             getNumberOne = GetOneIntKeyboard();
-            GetSquareNumber(getNumberOne); 
+            Print(GetSquareNumber(getNumberOne));
             break;
+        
         case (2):
-            Console.WriteLine("Task_4_2. Вывести на экран кубы чисел от A до B, которые вводит пользователь.");
-            Console.WriteLine("A: ");
+            Print("Task_4_2. Вывести на экран кубы чисел от A до B, которые вводит пользователь.");
+            Print("A: ");
             getNumberOne = GetOneIntKeyboard();
-            Console.WriteLine("B: ");
+            Print("B: ");
             getNumberTwo = GetOneIntKeyboard();
-            GetNumberCubed(getNumberOne, getNumberTwo);
+            Print(GetNumberCubed(getNumberOne, getNumberTwo));
+            
             break;
         case (3):
-            Console.WriteLine("Task_4_3 Написать программу на языке C#, которая возводит число в целочисленную степень. Число и степень вводятся с клавиатуры.");
-            Console.Write("Число: ");
+            Print("Task_4_3 Написать программу на языке C#, которая возводит число в целочисленную степень. Число и степень вводятся с клавиатуры.");
+            Print("Число: ");
             getDoubleOne = GetOneDoubleKeyboard();
-            Console.Write("Степень: ");
+            Print("Степень: ");
             getNumberOne = GetOneIntKeyboard();
-            GetExponentiation(getDoubleOne,getNumberOne);
+            Print(GetExponentiation(getDoubleOne,getNumberOne));
             break;
         case (4):
-            Console.WriteLine("Используя цикл написать программу, которая выводит на экран таблицу значений функции " +
+            Print("Используя цикл написать программу, которая выводит на экран таблицу значений функции " +
                               "y = 5 - x2/2 на отрезке [-5; 5] с шагом 0.5.");
-            GetTableFunction();
+            Print(" X      Y");
+            Print(GetTableFunction());
+            
             break;
         case (5):
-            Console.Write("Введите число от 0 до 255: ");
-            GetFactorial(GetOneByteKeyboard());
+            Print("Введите число от 0 до 255: ");
+            Print(GetFactorial(GetOneByteKeyboard()));
             break;
         case (6):
-            Console.WriteLine("Вывести на экран ряд чисел Фибоначчи, состоящий из N элементов. Значение N вводится с клавиатуры.");
-            GetNumbersFibonachi(GetOneIntKeyboard());
+            Print("Вывести на экран ряд чисел Фибоначчи, состоящий из N элементов. Значение N вводится с клавиатуры.");
+            Print(GetNumbersFibonachi(GetOneIntKeyboard()));
             break;
         case (7):
-            GetSeracuz();
+            Print("Проверка гипотезы Сиракуз.");
+            Print(GetSeracuz());
             break;
         case (8):
-            Console.WriteLine("Извлечение цифр числа.");
-            Console.Write("Введите целое число: ");
-            GetNumbers(GetOneIntKeyboard());
+            Print("Извлечение цифр числа.");
+            Print("Введите целое число: ");
+            Print(GetNumbers(GetOneIntKeyboard()));
             break;
         case (9):
             Environment.Exit(0);
@@ -86,85 +93,93 @@ void StartProgramm()
 }
 
 // 1
-void GetSquareNumber(int i)
+static string GetSquareNumber(int i)
 {
+    var sb = new StringBuilder();
     int  xNumber = 1, yNumber = 1;
       while (yNumber < i)
       {
-            Console.Write($"{yNumber} ");
+            sb.Append($"{yNumber} ");
             xNumber += 1;
             yNumber = (xNumber * xNumber);
       }
-    Console.WriteLine();
+    return sb.ToString();
 }
 
 // 2
-void GetNumberCubed(int aNumber,int bNumber)
+static string GetNumberCubed(int aNumber,int bNumber)
 {
+    var sb = new StringBuilder();
     for (int i = aNumber; i <= bNumber; i++)
-     Console.WriteLine($"{i} в кубе = {i*i*i}");
+     sb.Append($"{i} в кубе = {i*i*i}");
+    return sb.ToString();
 }
 
 // 3
-void GetExponentiation(double num, int deg)
+static string GetExponentiation(double num, int deg)
 {
+    var sb = new StringBuilder();
     double  res;
     res = 1;
     for (int i = 1; i <= Abs(deg); i++)
     res = res * num;
     if (deg < 0)
     res = 1 / res;
-    Console.WriteLine(res.ToString("0.000"));
+    sb.Append(res.ToString("0.000"));
+    return sb.ToString();
 }
 
 // 4
-void GetTableFunction()
+static string GetTableFunction()
 {
+    var sb = new StringBuilder();
     double x, y;
-    Console.WriteLine("Используя цикл написать программу, которая выводит на экран таблицу значений функции " +
-                      "y = 5 - x2/2 на отрезке [-5; 5] с шагом 0.5.");
     x = -5;
-    Console.WriteLine(" X      Y");
-
     while (x <= 5)
     {
         y = 5 - x * x / 2;
-        Console.WriteLine($"{x.ToString("0.0")} | {y.ToString("0.00")}");
+        sb.Append($"{x.ToString("0.0")} | {y.ToString("0.00")}");
         x = x + 0.5;
     }
+    return sb.ToString();
 }
 
 // 5
-void GetFactorial(int n)
+static string GetFactorial(int n)
 {  
+    var sb = new StringBuilder();
     Int64 factorial;
     factorial = 1;
     for (int i = 2; i <= n; i++)
         factorial = factorial * i;
-    Console.Write($"n != {factorial}" );
+    sb.Append($"n != {factorial}" );
+    return sb.ToString();
 }
 
 // 6
-void GetNumbersFibonachi(int n)
+static string  GetNumbersFibonachi(int n)
 {
+    var sb = new StringBuilder();
     int a, b, c;
      a = 0;
-    Console.Write($"{a} ");
+    sb.Append($"{a} ");
     b = 1;
-    Console.Write($"{b} ");
+    sb.Append($"{b} ");
     for (int i = 3; i <= n; i++)
     {
-        Console.Write($"{a+b} ");
+        sb.Append($"{a+b} ");
         c = b;
         b = a + b;
         a = c;
+        
     }   
+    return sb.ToString();
 }
 
 // 7
-void GetSeracuz()
+static string GetSeracuz()
 {
-    Console.WriteLine("Проверка гипотезы Сиракуз.");
+    var sb = new StringBuilder();
     short n;
     for (short i = 20; i <= 30; i++)
     {
@@ -175,21 +190,25 @@ void GetSeracuz()
             n = (short)(n / 2);
             else
             n = (short)((3 * n + 1) / 2); 
-            Console.Write($" |{n}|");
+            sb.Append($" |{n}|");
         }    
-        Console.WriteLine();
+        sb.Append("n/");
     }
+    return sb.ToString(); 
 }
 
 // 8
-void GetNumbers(int n)
+static string GetNumbers(int n)
 {
+    var sb = new StringBuilder();
     n = Abs(n);
     while (n != 0)
     {
-        Console.WriteLine(n % 10);
+        sb.Append(n % 10);
         n = n / 10;
     }
+
+    return sb.ToString();
 } 
 
 static int GetOneIntKeyboard()
@@ -238,4 +257,9 @@ static byte GetOneByteKeyboard()
         Console.WriteLine("Некорректное число.");
     } while (true);
     
+}
+
+void Print(object answer)
+{
+    Console.WriteLine(answer);
 }
